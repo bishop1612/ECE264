4c66445ee1419b8b429ac95b81978f748edf2189
Bishop <bsarkar@purdue.edu>
2014-09-14 23:17:08 -0400

    1st Function Success

--- PA03/answer03.c
+++ PA03/answer03.c
@@ -29,29 +29,29 @@ char * strcat_ex(char * * dest, int * n, const char * src)
 {
 	//int src_len = strlen(src);
 	//int dst_len = strlen(*dest);
-	//printf("En\n");
-printf("After swap, str1 == %p (i.e., '%s'), "
-       "str2 == %p (i.e., '%s')\n", src, src, *dest, *dest);
+	////printf("En\n");
+	//printf("After swap, str1 == %p (i.e., '%s'), "
+      // "str2 == %p (i.e., '%s')\n", src, src, *dest, *dest);
 	char *temp;
-	if((*dest == NULL) || (strlen(*dest) + strlen(src) + 1 > *n))
+	if((*dest == NULL) || ((*dest == NULL? 0:strlen(*dest)) + strlen(src) + 1 > *n))
 	{
-		//printf("INSIDE CONDITION STATEMENT\nSpace to be allocated %d\n", (int)((1 + 2 * ((*dest==NULL?0:strlen(*dest)) + strlen(src))) * sizeof(char )) );
-		temp = malloc((1 + 2 * ( (*dest==NULL?0:strlen(*dest)) + strlen(src))) * sizeof(char )) ;
+		temp = malloc((1 + 2 * ( (*dest==NULL?0:strlen(*dest)) + strlen(src)))) ;
 		*n = sizeof(temp);
 		//printf("value of n is %d\n", *n);
 		if(*dest != NULL)
 		{
 			strcpy(temp,*dest);
-			free(*dest);
+			//printf("temp's content after strcpy : %s\n", temp);
 		}
 		//printf("temp's content after strcpy : %s\n", temp);
+		//printf("temp's content after strcpy : %s\n", *dest);
+		free(dest);
+		//printf("temp's content after strcpy : %s\n", *dest);
+
 		// free(NULL) won't throw an error
-		*dest = (char *) &temp;
-		//free(temp);
+		*dest = temp;
 	}
 	*dest = strcat(*dest,src);
-	//printf("After swap, str1 == %p (i.e., '%s'), "
-       //"str2 == %p (i.e., '%s')\n", src, src, *dest, *dest);
 	return *dest;
 }
 /**
@@ -76,84 +76,49 @@ printf("After swap, str1 == %p (i.e., '%s'), "
  * Hint: this question is hard; it will help to draw out your algorithm.
  * Hint: read the FAQ...
  */
-char * * explode(const char * str, const char * delims, int * arrLen)
+/*char * * explode(const char * str, const char * delims, int * arrLen)
 {
-	return NULL;
-}
-
-/**
- * Takes an array of strings, and concatenates the elements into a single
- * string, placing 'glue' between each successive element.
- *
- * strArr: an array of strings
- * len: the length of the array 'strArr'
- * glue: string to concatenate between each element of 'strArr'
- *
- * For example:
- * int len;
- * char * * strArr = explode("100 224 147 80", " ", &len);
- * char * str = implode(strArr, len, ", ");
- * printf("(%s)\n", str); // (100, 224, 147, 80)
- *
- * Hint: use strcat_ex in a for loop.
- */
-char * implode(char * * strArr, int len, const char * glue)
-{
-	return NULL;
-}
-
-/**
- * Takes an array of C-strings, and sorts them alphabetically, ascending.
- *
- * arrString: an array of strings
- * len: length of the array 'arrString'
- *
- * For example, 
- * int len;
- * char * * strArr = explode("lady beatle brew", " ", &len);
- * sortStringArr, len);
- * char * str = implode(strArr, len, " ");
- * printf("%s\n"); // beatle brew lady
- *
- * Hint: use the <stdlib.h> function "qsort"
- * Hint: you must _clearly_ understand the typecasts.
- */
-void sortStringArray(char * * arrString, int len)
-{
-	return NULL;
-}
-/**
- * Sorts the characters in a string.
- *
- * str: string whose characters are to be sorted
- *
- * For example, 
- * char * s1 = strdup("How did it get so late so soon?");
- * sortStringCharacters(s1)
- * // s1 is now "       ?Haddeegiilnooooossstttw"
- *
- * Hint: use the <stdlib.h> function "qsort"
- * Hint: you must _clearly_  understand the typecasts.
- */
-void sortStringCharacters(char * str)
-{
-	return NULL;
-}
-/**
- * Safely frees all memory associated with strArr, and then strArr itself.
- * Passing NULL as the first parameter has no effect.
- *
- * strArr: an array of strings
- * len: the length of 'strArr'
- *
- * int len;
- * const char * abe = "Give me six hours to chop down a tree and I will spend\n"
- *                    "the first four sharpening the axe.";
- * char * * strArr = explode(abe, "\n ");
- * destroyStringArray(strArr, len); // cleans memory -- no memory leaks
- * destroyStringArray(NULL, 0); // does nothing, does not crash.
- */
-void destroyStringArray(char * * strArr, int len)
-{
-	return NULL;
-}
+	int N = 0;
+	int pos = 0;
+	int iter = 0;
+	while(str[pos] != '\0')
+	{
+		if(strchr(delims,str[pos]) != NULL)
+		{
+			N++;
+		}
+		pos++;
+	}
+	char * * strArr = malloc((N+1) * sizeof(char *)); 
+	pos = 0;
+	arrInd = 0; // this is the next position where we'll create a string
+	last = 0; // 1 + the last index we saw a delimiter. Init to 0.
+	while(str[pos] != '\0')
+	{
+		if(strchr(delims,str[pos]) != NULL)
+		{
+			char *temp;
+			while(last == pos)
+			{
+				temp[iter] = str[last];
+				last++;
+				iter++;
+			}
+			strArr[arrInd] = *temp;
+			arrInd++;
+			iter = 0;
+		}
+		pos++;
+	}
+	char *temp;
+	while(last == strlen(*str))
+	{
+		temp[iter] = str[last];
+		last++;
+		iter++;	
+	}
+	strArr[arrInd] = *temp;
+	*arrLen = strlen(**strArr);
+	
+	return **strArr;
+}*/
