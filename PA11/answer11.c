3dbe18ec8769f432e833e33a65926e700b8f7d99
Bishop <bsarkar@purdue.edu>
2014-12-02 19:25:45 -0500

    Ready for Testing

--- PA11/answer11.c
+++ PA11/answer11.c
@@ -3,6 +3,19 @@
 #include<ctype.h> 
 #include"answer11.h" 
 
+// -------------------------------------------------------------------- HuffNode
+
+/**
+ * A Huffman coding tree. We must implement two small functions... 
+ *
+ * (1) HuffNode_create(value);         // create a new HuffNode
+ * (2) HuffNode_destroy(tree);         // free all memory safely
+ */
+
+ /**
+ * Create a new, properly intialized HuffNode
+ */
+
 HuffNode * HuffNode_create(int value) 
 { 
 	HuffNode* hn = malloc(sizeof(HuffNode)); 
@@ -11,6 +24,10 @@ HuffNode * HuffNode_create(int value)
 	hn -> right = NULL; 
 	return(hn); 
 } 
+
+/**
+ * Destroy a tree, including all sub-children. Must handle NULL values safely.
+ */
 
 void HuffNode_destroy(HuffNode* tree) 
 { 
@@ -24,12 +41,36 @@ void HuffNode_destroy(HuffNode* tree)
 	free(tree); 
 }
 
+// ----------------------------------------------------------------------- Stack
+
+/**
+ * We need a "Stack" to build the tree structure that is used to decode
+ * a Huffman encoding. Stacks are very simple to implement with linked lists.
+ * We must implement the following functions (given below). Each of these 
+ * functions should only be a few lines of code.
+ *
+ * (1) Stack_create();              // Allocate a brand new stack.
+ * (2) Stack_destroy(stack);        // Clean up memory for the stack.
+ * (3) Stack_isEmpty(stack);    // TRUE iff (if and only if) the stack is empty.
+ * (4) Stack_pushFront(stack, tree); // Push a tree onto the stack.
+ * (6) Stack_popFront(stack);  // Remove a tree from the stack and return it.
+ *
+ * Altogether, these six functions should be around 40 lines of code.
+ */
+
+ /**
+ * Returns a pointer to a new empty stack struct
+ */
 Stack* Stack_create() 
 { 
 	Stack* stack = malloc(sizeof(Stack)); 
 	stack -> head = NULL; 
 	return(stack); 
 } 
+
+/**
+ * Returns TRUE (something other than zero) if the stack is empty.
+ */
 
 int Stack_isEmpty(Stack* stack) 
 { 
@@ -39,6 +80,12 @@ int Stack_isEmpty(Stack* stack)
 	} 
 	return(0); 
 } 
+
+/**
+ * Frees all memory associated with the stack. 
+ * Don't forget that you must free the entire contained linked-list.
+ * Also, you must safely do nothing if stack == NULL. 
+ */
 
 void Stack_destroy(Stack* stack) 
 { 
@@ -61,6 +108,15 @@ void Stack_destroy(Stack* stack)
 	free(stack); 
 	return; 
 } 
+
+/**
+ * Pop the front (top) 'value' (i.e. Huffman tree) from the stack.
+ *
+ * More precisely, this function must do three things:
+ * (1) Save the value (i.e. Huffman tree) of the head node of the stack's list
+ * (2) Remove the head node of the stack's list, freeing it.
+ * (3) Return the value (i.e. Huffman tree) saved in (1).
+ */
 HuffNode * Stack_popFront(Stack * stack) 
 { 
 	if(Stack_isEmpty(stack)) 
@@ -70,10 +126,20 @@ HuffNode * Stack_popFront(Stack * stack)
 	} 
 	HuffNode* tree = stack -> head -> tree; 
 	StackNode* temp = stack -> head -> next; 
-	free(stack -> head); 
+	free(stack
+	 -> head); 
 	stack -> head = temp; 
 	return(tree); 
 } 
+
+/**
+ * Push a 'value' (i.e. Huffman tree) onto the front (top) of the stack.
+ *
+ * More precisely, this function must do two things:
+ * (1) Create a new StackNode with 'tree' for its tree.
+ * (2) Push that new StackNode onto the front of the stack's list.
+ */
+
 Stack* Stack_push(Stack* stack,HuffNode* tree) 
 { 
 	StackNode* tmp = malloc(sizeof(StackNode)); 
@@ -88,10 +154,22 @@ Stack* Stack_push(Stack* stack,HuffNode* tree)
 	tmp -> next = final; // And point 'temp -> next' to the 'stack -> head'. 
 	return(stack); 
 } 
+
 void Stack_pushFront(Stack* stack,HuffNode* tree) 
 { 
 	stack = Stack_push(stack,tree); 
 } 
+
+// -------------------------------------------------------- Pop Pop combine Push
+
+/**
+ * This function helps simplify building a Huffman Coding Tree from the header
+ * information. It takes a stack as input. As a precondition, you can assume 
+ * that the stack has at least two nodes. This function pops the front (top) 
+ * two nodes, combines them into a single node, and pushes the new node back 
+ * onto the stack. See Huffman_Coding.pdf to understand conceptually how this
+ * should be done.
+ */
 void Stack_popPopCombinePush(Stack * stack) 
 { 
 	HuffNode* right = Stack_popFront(stack); 
@@ -102,6 +180,12 @@ void Stack_popPopCombinePush(Stack * stack)
 	temp -> right = right; // Sending in the first popped node into the right node of the Huffman Tree 
 	Stack_pushFront(stack,temp); // Pushing the Newly made tree. 
 }
+
+// ---------------------------------------------------- Reading HuffTree headers
+
+/**
+ * Read a Huffman Coding Tree (in text format) from 'fp'.
+ */
 HuffNode* HuffTree_readTextHeader(FILE* fp) 
 { 
 	if(fp == NULL) 
@@ -135,19 +219,25 @@ HuffNode* HuffTree_readTextHeader(FILE* fp)
 			return(final); 
 }
 
+/**
+ * Read a Huffman Coding Tree (in binary format) from 'fp'.
+ * You will need to (conceptually) read a file a bit at a time. See the README
+ * for hints on how to do this.
+ */
+
 // This Function is taken from Yung Lu's Book
 int readBit(FILE* fptr, unsigned char* bit, unsigned char* bitcount, unsigned char* currentbyte)
 {
-	int ret = 1;
+	int rem = 1;
 
 	if(*bitcount == 0)
 	{
-		ret = fread(currentbyte, sizeof(unsigned char),1,fptr);
+		rem = fread(currentbyte, sizeof(unsigned char),1,fptr);
 	}
 
 
 	// If the fread fails
-	if(ret != 1)
+	if(rem != 1)
 	{
 		return(-1);
 	}
@@ -185,7 +275,8 @@ HuffNode* HuffTree_readBinaryHeader(FILE* fp)
 		readBit(fp,&onebit,&bitcount,&currentbyte);
 
 		if(onebit == 1)
-		{ // a leaf node , get 7 move bits
+		{ 
+			// a leaf node , get 7 move bits
 
 			int count;
 			unsigned char val = 0;
